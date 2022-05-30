f93674f126b18944695e522c453628a9fcaa7114
Daiana Ghisio <dghisio@avalian.com>
2022-05-30 15:39:20 -0300

    class created

--- force-app/main/default/aura/CardButton/CardButtonController.js
+++ force-app/main/default/aura/CardButton/CardButtonController.js
@@ -1,5 +1,25 @@
 ({
     handleClick : function(component, event, helper) {
+        if(parseJSONResponse()==true){
 
+            LightningUtils.showToast(
+                "Success!",
+                "The color has been saved successfully",
+                {type: "success"}
+            );
+
+
+        } // if true ends 
+        else {
+
+            LightningUtils.showToast(
+                "Error!",
+                "The color has not been saved.",
+                {type: "error"}
+            );
+
+
+
+        } //else ends
     }
 })
