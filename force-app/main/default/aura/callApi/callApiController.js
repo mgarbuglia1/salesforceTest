7580341b71f7acd3c5c79f569083263e0859e82f
mariana garbuglia <mgarbuglia@avalian.com>
2022-06-03 12:29:31 -0300

    Se agregaron las clases de Test y de MockTest.

--- force-app/main/default/aura/callApi/callApiController.js
+++ force-app/main/default/aura/callApi/callApiController.js
@@ -1,19 +1,44 @@
 ({
     handleClick : function(component, event, helper) {
-        if(ClassCall()==true){
-            LightningUtils.showToast(
-                "Correcto",
-                "New color added!",
-                {type: "success"}
-            );
-        }
-        else
-        {
-            LightningUtils.showToast(
-                "Error",
-                "No color to Show!",
-                {type: "error"}
-            );
-        }
+
+        var action = component.get("c.CallApi");
+        var currentRecordId = component.get('v.getRecordId');
+        action.setParams({ AccountId : currentRecordId });
+
+        action.setCallback(this, function(response) {
+            var color = response.getReturnValue();
+            if (color.successful === "true") {
+                //SUCCESS
+                // Alert the user with the value returned from the server
+                //aura show toast
+                showToastTrue();
+            }
+            else{
+                showToastFalse();
+            }
+        });
+
+        // $A.enqueueAction adds the server-side action to the queue.
+        $A.enqueueAction(action);
     }
+},
+{
+    showToastTrue : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Correcto!",
+            "message": "New color added!"
+        });
+        toastEvent.fire();
+    }
+},
+{
+    showToastFalse : function(component, event, helper) {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Correcto!",
+            "message": "New color added!"
+        });
+        toastEvent.fire();
+    } 
 })
