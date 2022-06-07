f38e655e2c3030c5c9e05a6448e5d0fcf0ad791c
mariana garbuglia <mgarbuglia@avalian.com>
2022-06-07 12:36:01 -0300

    div para mostrar el color. Test para testGetLatest

--- force-app/main/default/aura/callApi/callApiController.js
+++ force-app/main/default/aura/callApi/callApiController.js
@@ -13,6 +13,7 @@
                 //aura show toast
 
                 helper.showToast('Correcto!','New color added!');
+                //$A.doInit();
             }
             else{
                 helper.showToast('Error!','Se produjo un error al intentar agregar un color');
@@ -25,8 +26,39 @@
     
     //evento de inicio
     doInit : function(component, event, helper) {
-		//console.log('Aura Component Loaded');
-        helper.showToastEmpty();
+
+        var actionMostrar = component.get("c.getLatest");
+        var currentRecordId = component.get('v.getRecordId');
+        actionMostrar.setParams({ accountId : currentRecordId });
+
+        actionMostrar.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log(JSON.parse(JSON.stringify(response.getReturnValue()))); 
+
+                component.set('v.codigo', response.getReturnValue().colorCode__c);
+
+
+                helper.showToast('Correcto!','New color added!');
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+                helper.showToastEmpty();
+
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            } 
+        });
+        $A.enqueueAction(actionMostrar);
 	}
 }
 )
