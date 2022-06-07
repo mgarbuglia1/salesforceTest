0d0f365b9ca5683e5fb01765a71cda7ac330ed93
Daiana <dghisio@avalian.com>
2022-06-07 15:58:54 -0300

    It works

--- force-app/main/default/aura/CardButton/CardButtonController.js
+++ force-app/main/default/aura/CardButton/CardButtonController.js
@@ -2,23 +2,22 @@
   
     handleClick : function(component, event, helper) {
 
+
         var action = component.get("c.getColor"); 
         action.setParams( { cuentaId : currentRecordId });
 
-        var currentRecordId =  component.get("v.recordId");
+        var currentRecordId =  component.get("v.recordId"); //ubicacion
 
         action.setCallback(this, function(response) {
+            
             var color = response.getReturnValue();
             if (color.successful === "true") {
                 
-                helper.showToastTrue();
+                helper.showToast('Success!', 'The record has been updated successfully :)' );
 
-             // $A.doInit(); //PREGUNTAR
-
-               
             } else if (color.successful === "false") {
 
-                helper.showToastFalse();
+                helper.showToast('Error.', 'The record has NOT been updated successfully :(');
             }
             
         });
@@ -30,25 +29,43 @@
 
     doInit: function(component, event, helper) {
         
-        var action = component.get("c.getLatest"); 
+      
+        var action = component.get("c.getLatest");
+        var currentRecordId =  component.get("v.recordId"); //Por que esta ubicado asi
         action.setParams( { cuentaId : currentRecordId });
-
-        var currentRecordId =  component.get("v.recordId");
+       
 
         action.setCallback(this, function(response) {
-            var colorL = response.getReturnValue();
-
-            if(colorL===null){
-
-                helper.showToastEmpty();
-
-            } else {
-
-             var colorCode = colorL.colorCode;
-             var colorName = colorL.colorName;
-            
-            //enviar colorCode al atributo codigo del componente
-            }            
+           
+            var state = response.getState();
+            var color = response.getReturnValue();
+          
+            if (state === "SUCCESS") {
+               
+                if(color==null){
+                    helper.showToastEmpty();                    
+                } else {
+                    console.log(JSON.parse(JSON.stringify(response.getReturnValue()))); 
+                      component.set('v.codigo', response.getReturnValue().colorCode__c);
+                      helper.showToast('Genial!!','Recibimos el color más reciente correctamente!'); 
+                }
+              
+            }
+            else if (state === "INCOMPLETE") {
+                // do something
+            }
+            else if (state === "ERROR") {
+             
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            } 
         });
 
     $A.enqueueAction(action);
--- force-app/main/default/aura/CardButton/CardButtonHelper.js
+++ force-app/main/default/aura/CardButton/CardButtonHelper.js
@@ -1,25 +1,14 @@
 ({
    
-        showToastTrue : function(component, event, helper) {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                "title": "Success!",
-                "message": "The record has been updated successfully."
-            });
-            toastEvent.fire();
-        },   
+    showToast: function(titulo, mensaje, tipo) {
+        var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": titulo,
+                    "message": mensaje
+                });
+                toastEvent.fire();
+    }, 
     
-        showToastFalse : function(component, event, helper) {
-            var toastEvent = $A.get("e.force:showToast");
-            toastEvent.setParams({
-                "title": "Error!",
-                "message": "The record has NOT been updated successfully."
-            });
-            toastEvent.fire();
-        },   
-    
-        //se llama este toast desde doInit
-        //funciona el toast, falta agregar logica para chequear si hay o no un color
         showToastEmpty : function(component, event, helper) {
             var toastEvent = $A.get("e.force:showToast");
             toastEvent.setParams({
@@ -28,5 +17,5 @@
             });
             toastEvent.fire();
         }
-    
+
 })
